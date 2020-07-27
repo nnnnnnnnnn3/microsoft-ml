6b58fdd5af86d47ffe73a7621118a117a2121220
x-rw <acrn3w@gmail.com>
2020-07-27 00:20:46 -0400

    chapter 3

---
+++ teory/model training.mm
@@ -0,0 +1,182 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1595819591914" ID="ID_1250400034" MODIFIED="1595819611244" TEXT="model training">
+<node CREATED="1595819620158" ID="ID_1952798451" MODIFIED="1595819811944" POSITION="right" TEXT="data import and transformation">
+<richcontent TYPE="NOTE">
+  
+    
+    
+  
+  
+    
+      
+        
+          
+            
+              
+                Data wrangling&#160;is the process of cleaning and 
+                transforming data to make it more appropriate for data 
+                analysis. The process generally follows these main steps:
+              
+              
+                
+                  Explore the raw data and check the general quality of the 
+                  dataset.
+                
+                
+                  Transform the raw data, by restructuring, normalizing, and 
+                  cleaning the data. For example, this could involve handling 
+                  missing values and detecting errors.
+                
+                
+                  Validate and publish the data.
+                
+              
+              
+                Data wrangling is an&#160;iterative&#160;process 
+                where you do some data transformation then check the results 
+                and come back to the process to make improvements.
+              
+            
+          
+        
+      
+    
+    
+      
+        
+          
+            
+              
+                
+                  QUIZ QUESTION
+                
+                
+                  
+                    See if you can match the following descriptions with the 
+                    data wrangling task they describe.
+                  
+                
+              
+              
+                
+                  
+                    Submit to check your answer choices!
+                  
+                
+              
+              
+                
+                  
+                
+                
+                  
+                    DESCRIPTION
+                  
+                
+                
+                  
+                
+                
+                  
+                    TASK
+                  
+                
+              
+              
+                
+                  
+                    
+                      
+                        Counting the number of missing values
+                      
+                    
+                  
+                  
+                    
+                      
+                        
+                          
+                            Data discovery and exploration
+                          
+                        
+                      
+                    
+                  
+                
+                
+                  
+                    
+                      
+                        Missing values imputation
+                      
+                    
+                  
+                  
+                    
+                      
+                        
+                          
+                            Data cleansing
+                          
+                        
+                      
+                    
+                  
+                
+                
+                  
+                    
+                      
+                        Normalize feature values
+                      
+                    
+                  
+                  
+                    
+                      
+                        
+                          
+                            Data restructuring
+                          
+                        
+                      
+                    
+                  
+                
+              
+            
+            
+              
+              SUBMIT
+            
+          
+        
+      
+    
+    
+      
+        
+          
+            
+              
+                The best way to understanding data wrangling is to get 
+                hands-on experience actually doing it&#8212;so that's what we'll do 
+                in our next lab.
+              
+            
+          
+        
+      
+    
+    
+
+    
+      
+    
+  
+
+</richcontent>
+</node>
+</node>
+</map>
